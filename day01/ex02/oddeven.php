7cd3990892c2106776dbb205c58ce25cab7a583e
sleepychaman <ryanbi91420@gmail.com>
2017-10-03 16:54:36 +0200

    ok

---
+++ day01/ex02/oddeven.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+
+while(42)
+{
+    echo "Entrez un nombre: ";
+    $line = trim(fgets(STDIN));
+    if (feof(STDIN) == TRUE)
+       exit();
+    if (is_numeric($line) == FALSE)
+        echo "\n'$line' n'est pas un chiffre\n";
+    else if (($line % 2) == 0)
+        echo "\nLe chiffre $line est Pair\n";
+    else
+        echo "\nLe chiffre $line est Impair\n";
+}
+?>
